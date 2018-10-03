56480c8f6ecb52c7a088023a3ddb2856876477d4
Dominic Njue <njuemugodominic@gmail.com>
2018-10-04 00:03:29 +0300

    changes the center of map

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var map;
 require(["esri/map", "dojo/domReady!"], function(Map) {
 map = new Map("map", {
   basemap: "topo",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
-  center: [-122.45, 37.75], // longitude, latitude
-  zoom: 13
+  center: [32.969565, 0.0649221], // longitude, latitude
+  zoom: 6
 });
 });
